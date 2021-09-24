4a60697538a07e12e1c7b24eda3392a4cffec3b5
sohamsshah <sohamshah456@gmail.com>
2021-09-24 23:12:42 +0530

    :art: add error messages and refactor code

--- e-commerce/src/backend/controllers/AuthController.js
+++ e-commerce/src/backend/controllers/AuthController.js
@@ -13,34 +13,46 @@ const jwt = require("jsonwebtoken");
  * body contains {firstName, lastName, email, password}
  * */
 
-export const signupHandler = function(schema, request) {
+export const signupHandler = function (schema, request) {
   const { email, password, firstName, lastName } = JSON.parse(
     request.requestBody
   );
-  // check if email already exists
-  const foundUser = schema.users.findBy({ email: email });
-  if(foundUser){
-    return new Response(422, {}, {
-      errors: [
-        "Unprocessable Entity. Email Already Exists.",
-      ],
-    } );
+  try {
+    // check if email already exists
+    const foundUser = schema.users.findBy({ email: email });
+    if (foundUser) {
+      return new Response(
+        422,
+        {},
+        {
+          errors: ["Unprocessable Entity. Email Already Exists."],
+        }
+      );
+    }
+    const newUser = {
+      cart: [],
+      wishList: [],
+      email,
+      firstName,
+      lastName,
+      password,
+      _id: uuid(),
+    };
+    const createdUser = schema.users.create(newUser);
+    const encodedToken = jwt.sign(
+      { email, password },
+      process.env.REACT_APP_JWT_SECRET
+    );
+    return new Response(201, {}, { createdUser, encodedToken });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
   }
-  const newUser = {
-    cart: [],
-    wishList: [],
-    email,
-    firstName,
-    lastName,
-    password,
-    _id: uuid(),
-  };
-  const createdUser = schema.users.create(newUser);
-  const encodedToken = jwt.sign(
-    { email, password },
-    process.env.REACT_APP_JWT_SECRET
-  );
-  return new Response(201, {}, { createdUser, encodedToken });
 };
 
 /**
@@ -49,24 +61,42 @@ export const signupHandler = function(schema, request) {
  * body contains {email, password}
  * */
 
-export const loginHandler = function(schema, request){
+export const loginHandler = function (schema, request) {
   const { email, password } = JSON.parse(request.requestBody);
-  const encodedToken = jwt.sign(
-    { email, password },
-    process.env.REACT_APP_JWT_SECRET
-  );
-  const foundUser = schema.users.findBy({ email: email });
-  if (foundUser) {
-    if (foundUser.password === password) {
-      return new Response(200, {}, { foundUser, encodedToken });
+  try {
+    const encodedToken = jwt.sign(
+      { email, password },
+      process.env.REACT_APP_JWT_SECRET
+    );
+    const foundUser = schema.users.findBy({ email: email });
+    if (foundUser) {
+      if (foundUser.password === password) {
+        return new Response(200, {}, { foundUser, encodedToken });
+      }
+      return new Response(
+        404,
+        {},
+        {
+          errors: ["The email you entered is not Registered. Not Found error"],
+        }
+      );
     }
-    return new Response(404, {}, {
-      errors: ["The email you entered is not Registered. Not Found error"],
-    });
+    return new Response(
+      401,
+      {},
+      {
+        errors: [
+          "The credentials you entered are incorrect. Unauthorized access error.",
+        ],
+      }
+    );
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
   }
-  return new Response(401, {}, {
-    errors: [
-      "The credentials you entered are incorrect. Unauthorized access error.",
-    ],
-  });
 };
--- e-commerce/src/backend/controllers/CategoryController.js
+++ e-commerce/src/backend/controllers/CategoryController.js
@@ -10,17 +10,27 @@ import { Response } from "miragejs";
  * send GET Request at /api/categories
  * */
 
-export const getAllCategoriesHandler = function (){
-    return new Response(200, {}, { categories: this.db.categories });
-}
+export const getAllCategoriesHandler = function () {
+  return new Response(200, {}, { categories: this.db.categories });
+};
 
 /**
  * This handler handles gets all categories in the db.
  * send GET Request at /api/user/category/:categoryId
  * */
 
-export const getCategoryHandler =  function (schema, request) {
-    const categoryId = request.params.categoryId;
-    const category = this.db.categories.findBy({_id: categoryId});
+export const getCategoryHandler = function (schema, request) {
+  const categoryId = request.params.categoryId;
+  try {
+    const category = this.db.categories.findBy({ _id: categoryId });
     return new Response(200, {}, { category });
-}
+  } catch (error) {
+    new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
