5511afed1a2d48c60f4f61a58401cb0d05d6ff07
Soham Shah <47717492+sohamsshah@users.noreply.github.com>
2021-10-04 16:47:15 +0530

    Npx starter (#22)
    * :art: initialize npx starter
    
    * :construction: update file structure WIP
    
    * :sparkles: complete initial cli npx starter kit, powered by rollup
    
    * :memo: update package.json
    
    * :bug: add error handling for index.js

---
+++ bin.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+
+const main = require('./dist/main');
+main()
+	.then(() => process.exit(0))
+	.catch((err) => {
+		console.error(err);
+		process.exit(1);
+	});
---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+// rollup.config.js
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/init.js',
+  output: {
+    file: 'dist/main.js',
+    format: 'cjs'
+  },
+  
+  plugins: [nodeResolve({
+    // pass custom options to the resolve plugin
+    customResolveOptions: {
+      moduleDirectories: ['node_modules']
+    }
+  }), terser(), commonjs()],
+};
