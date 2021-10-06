affe8a512a527abbb266d3128bfe4beea300cb09
sohamsshah <sohamshah456@gmail.com>
2021-10-06 16:30:15 +0530

    :white_check_mark: complete authentication routes

--- apps/social-media/src/App.js
+++ apps/social-media/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+// THIS FILE IS JUST FOR TESTING PURPOSES. Will be deleted once while publishing it.
+
+import React,{useState} from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  let [token, setToken] = useState("");
+  // signup API call
+  const signupHandler = async () => {
+    try {
+      const response = await axios.post(
+        `/api/auth/signup`, {firstName: "Soham",
+        lastName: "Shah",
+        email: "sohamshah456@gmail.com",
+        password: "123"}
+      );
+      localStorage.setItem("token", response.data.encodedToken);
+        setToken(response.data.encodedToken);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // login API call
+  const loginHandler = async () => {
+    try {
+      const response = await axios.post(
+        `/api/auth/login`, {
+        email: "sohamshah456@gmail.com",
+        password: "123"}
+      );
+      localStorage.setItem("token", response.data.encodedToken);
+        setToken(response.data.encodedToken);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={signupHandler}>Signup</button>   
+      <button onClick={loginHandler}>Login</button>
     </div>
   );
 }
---
+++ apps/social-media/src/server.js
@@ -0,0 +1,39 @@
+import { Server, Model, RestSerializer } from "miragejs";
+import { users } from "./backend/db/users";
+import {
+  loginHandler,
+  signupHandler,
+} from "./backend/controllers/AuthController";
+
+export function makeServer({ environment = "development" } = {}) {
+  let server = new Server({
+    serializers: {
+      application: RestSerializer,
+    },
+    environment,
+    // TODO: Use Relationships to have named relational Data
+    models: {
+      product: Model,
+      wishList: Model,
+      cart: Model,
+      user: Model,
+      category: Model,
+    },
+
+    // Runs on the start of the server
+    seeds(server) {
+
+      users.forEach((item) =>
+        server.create("user", { ...item, cart: [], wishList: [] })
+      );
+    },
+
+    routes() {
+      this.namespace = "api";
+      // auth routes (public)
+      this.post("/auth/signup", signupHandler.bind(this));
+      this.post("/auth/login", loginHandler.bind(this));
+    },
+  });
+  return server;
+}
