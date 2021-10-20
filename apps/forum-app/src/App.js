8061ca711a14e68ef93db8fcba47982b3573af59
sohamsshah <sohamshah456@gmail.com>
2021-10-20 11:23:37 +0530

    :zap: complete user routes

--- apps/forum-app/src/App.js
+++ apps/forum-app/src/App.js
@@ -4,7 +4,48 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'
 export default function App() {
   let [token, setToken] = useState("");
+  let [users, setUsers] = useState([]);
   const encodedToken = localStorage.getItem("token");
+
+  useEffect(() => {
+    (async () => {
+      await handleFetchUsers();
+      await fetchUserDetails("92fefa34-e003-4d64-a99f-22e25479cbde");
+    })();
+  }, []);
+
+  const handleEditUser = async (userData) => {
+    try {
+      const response = await axios.post(
+        `/api/users/edit`,
+        {userData},
+        {
+          headers: {
+            authorization: encodedToken,
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleFetchUsers = async () => {
+    try {
+      const response = await axios.get(`/api/users`);
+      setUsers(response.data.users);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchUserDetails = async (userId) => {
+    try {
+      const response = await axios.get(`/api/users/${userId}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   
   // signup API call
   const signupHandler = async () => {
@@ -42,6 +83,7 @@ export default function App() {
       <h2>Available Products</h2>
       <button onClick={() => signupHandler()}> Signup</button>
       <button onClick={() => loginHandler()}> Login</button>
+      <button onClick={() => handleEditUser({ firstName: "sodium", lastName:"lol" })}>Edit details</button>
     </div>
   );
 }
--- apps/forum-app/src/server.js
+++ apps/forum-app/src/server.js
@@ -2,6 +2,7 @@ import {
     loginHandler,
     signupHandler,
   } from "./backend/controllers/AuthController";
+import { getAllUsersHandler, getUserHandler, editUserHandler } from "./backend/controllers/UserController";
 import { Server, Model, RestSerializer } from "miragejs";
 import { users } from "./backend/db/users";
 export function makeServer({ environment = "development" } = {}) {
@@ -26,6 +27,14 @@ export function makeServer({ environment = "development" } = {}) {
         // auth routes (public)
         this.post("/auth/signup", signupHandler.bind(this));
         this.post("/auth/login", loginHandler.bind(this));
+
+        // user routes (public)
+        this.get("/users", getAllUsersHandler.bind(this));
+        this.get("/users/:userId", getUserHandler.bind(this));
+
+       // user routes (private)
+       this.post("users/edit", editUserHandler.bind(this))
+
   
     },
     });
