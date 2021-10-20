114f0744aad6323422a766f57cf1b75e2eb9cb05
sohamsshah <sohamshah456@gmail.com>
2021-10-20 10:57:12 +0530

    :lock: complete auth APIs

--- apps/forum-app/src/App.js
+++ apps/forum-app/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// THIS FILE IS JUST FOR TESTING PURPOSES. Will be deleted once while publishing it.
 
-function App() {
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+export default function App() {
+  let [token, setToken] = useState("");
+  const encodedToken = localStorage.getItem("token");
+  
+  // signup API call
+  const signupHandler = async () => {
+    try {
+      const response = await axios.post(
+        `/api/auth/signup`, {firstName: "Soham",
+        lastName: "Shah",
+        email: "sohamshah456@gmail.com",
+        password: "123"}
+      );
+      localStorage.setItem("token", response.data.encodedToken);
+        setToken(response.data.encodedToken);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // login API call
+  const loginHandler = async () => {
+    try {
+      const response = await axios.post(
+        `/api/auth/login`, {
+        email: "sohamshah456@gmail.com",
+        password: "123"}
+      );
+      localStorage.setItem("token", response.data.encodedToken);
+        setToken(response.data.encodedToken);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Available Products</h2>
+      <button onClick={() => signupHandler()}> Signup</button>
+      <button onClick={() => loginHandler()}> Login</button>
     </div>
   );
 }
-
-export default App;
--- apps/forum-app/src/index.js
+++ apps/forum-app/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { makeServer } from "./server";
+
+// Call make Server
+makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +14,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
