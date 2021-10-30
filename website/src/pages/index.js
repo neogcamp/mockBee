9b80fb3a125209acee2a53609c4f75c104f08716
Soham Shah <47717492+sohamsshah@users.noreply.github.com>
2021-10-30 23:08:11 +0530

    Update Documentation Site (#53)
    * :lipstick: update docsite
    
    * :page_facing_up: update docsite and ui
    
    * add favicon

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import clsx from 'clsx'
-import Layout from '@theme/Layout'
-import Link from '@docusaurus/Link'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import styles from './index.module.css'
-import HomepageFeatures from '../components/HomepageFeatures'
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+import HomepageFeatures from "../components/HomepageFeatures";
 
-function HomepageHeader () {
-  const { siteConfig } = useDocusaurusContext()
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
+        <img src="img/mockbee.png" width="180" height="180" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
@@ -20,14 +21,20 @@ function HomepageHeader () {
           >
             Get Started
           </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/api/introduction"
+          >
+            API Documentation
+          </Link>
         </div>
       </div>
     </header>
-  )
+  );
 }
 
-export default function Home () {
-  const { siteConfig } = useDocusaurusContext()
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
@@ -36,5 +43,5 @@ export default function Home () {
       <HomepageHeader />
       <main>{/* <HomepageFeatures /> */}</main>
     </Layout>
-  )
+  );
 }
