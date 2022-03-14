6ca60d72291cbdf0010dc96e2d77b0a1f8befa82
sohamsshah <sohamshah456@gmail.com>
2022-03-15 00:38:42 +0530

    feat: complete video library

--- apps/video-library/src/backend/controllers/AuthController.js
+++ apps/video-library/src/backend/controllers/AuthController.js
@@ -1,9 +1,7 @@
 import { v4 as uuid } from "uuid";
 import { Response } from "miragejs";
 import { formatDate } from "../utils/authUtils";
-import bcrypt from "bcryptjs";
-const jwt = require("jsonwebtoken");
-
+const sign = require("jwt-encode");
 /**
  * All the routes related to Auth are present here.
  * These are Publicly accessible routes.
@@ -30,11 +28,10 @@ export const signupHandler = function (schema, request) {
       );
     }
     const _id = uuid();
-    const encryptedPassword = bcrypt.hashSync(password, 5);
     const newUser = {
       _id,
       email,
-      password: encryptedPassword,
+      password,
       createdAt: formatDate(),
       updatedAt: formatDate(),
       ...rest,
@@ -43,10 +40,7 @@ export const signupHandler = function (schema, request) {
       playlists: [],
     };
     const createdUser = schema.users.create(newUser);
-    const encodedToken = jwt.sign(
-      { _id, email },
-      process.env.REACT_APP_JWT_SECRET
-    );
+    const encodedToken = sign({ _id, email }, process.env.REACT_APP_JWT_SECRET);
     return new Response(201, {}, { createdUser, encodedToken });
   } catch (error) {
     return new Response(
@@ -76,8 +70,8 @@ export const loginHandler = function (schema, request) {
         { errors: ["The email you entered is not Registered. Not Found error"] }
       );
     }
-    if (bcrypt.compareSync(password, foundUser.password)) {
-      const encodedToken = jwt.sign(
+    if (password === foundUser.password) {
+      const encodedToken = sign(
         { _id: foundUser._id, email },
         process.env.REACT_APP_JWT_SECRET
       );
