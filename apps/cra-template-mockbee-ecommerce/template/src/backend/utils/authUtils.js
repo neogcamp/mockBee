eb69cca4a196a46141b2891ad4380551dccda6f9
sohamsshah <sohamshah456@gmail.com>
2022-03-14 22:31:18 +0530

    fix: ecommerce app related errors and templates

--- apps/cra-template-mockbee-ecommerce/template/src/backend/utils/authUtils.js
+++ apps/cra-template-mockbee-ecommerce/template/src/backend/utils/authUtils.js
@@ -1,10 +1,10 @@
 import { Response } from "miragejs";
-const jwt = require("jsonwebtoken");
 import dayjs from "dayjs";
+import jwt_decode from "jwt-decode";
 
 export const requiresAuth = function (request) {
   const encodedToken = request.requestHeaders.authorization;
-  const decodedToken = jwt.verify(
+  const decodedToken = jwt_decode(
     encodedToken,
     process.env.REACT_APP_JWT_SECRET
   );
