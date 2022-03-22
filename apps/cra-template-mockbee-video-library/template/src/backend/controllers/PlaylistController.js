d70ff26faac40bf12e9e3e7768a2ff18f03cde40
Saurabh Suthar <launchpad5682@gmail.com>
2022-03-23 00:28:10 +0530

    fix: getVideosFromPlaylistHandler fixed
    getVideosFromPlaylistHandler had a wrong check while finding the videos for a playlist. It was returning videos whose video id is not equal to playlist ID. So, just changed !== to ===.

--- apps/cra-template-mockbee-video-library/template/src/backend/controllers/PlaylistController.js
+++ apps/cra-template-mockbee-video-library/template/src/backend/controllers/PlaylistController.js
@@ -89,7 +89,7 @@ export const getVideosFromPlaylistHandler = function (schema, request) {
   const user = requiresAuth.call(this, request);
   if (user) {
     const playlistId = request.params.playlistId;
-    const playlist = user.playlists.find((item) => item._id !== playlistId);
+    const playlist = user.playlists.find((item) => item._id === playlistId);
     return new Response(200, {}, { playlist });
   }
   return new Response(
