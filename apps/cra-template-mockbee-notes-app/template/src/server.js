67325e7572a23199ccdbfe0bb67859e822491d36
Nada Farook <nadaaofficial19@gmail.com>
2022-04-04 23:36:58 +0530

    Added trash api in backend and updated template

--- apps/cra-template-mockbee-notes-app/template/src/server.js
+++ apps/cra-template-mockbee-notes-app/template/src/server.js
@@ -15,6 +15,11 @@ import {
   getAllNotesHandler,
   updateNoteHandler,
 } from "./backend/controllers/NotesController";
+import {
+  deleteFromTrashHandler,
+  getAllTrashNotesHandler,
+  restoreFromTrashHandler,
+} from "./backend/controllers/TrashController";
 import { users } from "./backend/db/users";
 
 export function makeServer({ environment = "development" } = {}) {
@@ -36,6 +41,7 @@ export function makeServer({ environment = "development" } = {}) {
           ...item,
           notes: [],
           archives: [],
+          trash: [],
         })
       );
     },
@@ -63,6 +69,10 @@ export function makeServer({ environment = "development" } = {}) {
         "/archives/delete/:noteId",
         deleteFromArchivesHandler.bind(this)
       );
+      // trash routes (private)
+      this.get("/trash", getAllTrashNotesHandler.bind(this));
+      this.post("/trash/restore/:noteId", restoreFromTrashHandler.bind(this));
+      this.delete("/trash/delete/:noteId", deleteFromTrashHandler.bind(this));
     },
   });
   return server;
--- apps/notes-app/src/backend/controllers/AuthController.js
+++ apps/notes-app/src/backend/controllers/AuthController.js
@@ -38,6 +38,7 @@ export const signupHandler = function (schema, request) {
       ...rest,
       notes: [],
       archives: [],
+      trash: [],
     };
     const createdUser = schema.users.create(newUser);
     const encodedToken = sign({ _id, email }, process.env.REACT_APP_JWT_SECRET);
