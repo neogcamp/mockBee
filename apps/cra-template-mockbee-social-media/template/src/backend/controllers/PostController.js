49343b1a765162bb04b6d2cd326db9ed921fbe41
Hamza <hamzararrani77@gmail.com>
2022-05-15 10:48:57 +0530

    fix: Fixed the issue in social-media template related to formation of circular json

--- apps/cra-template-mockbee-social-media/template/src/backend/controllers/PostController.js
+++ apps/cra-template-mockbee-social-media/template/src/backend/controllers/PostController.js
@@ -180,7 +180,7 @@ export const likePostHandler = function (schema, request) {
       (currUser) => currUser._id !== user._id
     );
     post.likes.likeCount += 1;
-    post.likes.likedBy.push(user);
+    post.likes.likedBy.push({...user, bookmarks: []});
     this.db.posts.update({ _id: postId }, { ...post, updatedAt: formatDate() });
     return new Response(201, {}, { posts: this.db.posts });
   } catch (error) {
@@ -233,7 +233,7 @@ export const dislikePostHandler = function (schema, request) {
     const updatedLikedBy = post.likes.likedBy.filter(
       (currUser) => currUser._id !== user._id
     );
-    post.likes.dislikedBy.push(user);
+    post.likes.dislikedBy.push({...user, bookmarks: []});
     post = { ...post, likes: { ...post.likes, likedBy: updatedLikedBy } };
     this.db.posts.update({ _id: postId }, { ...post, updatedAt: formatDate() });
     return new Response(201, {}, { posts: this.db.posts });
