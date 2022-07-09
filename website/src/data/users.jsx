0a2742d0ed3e450c9b8284a54fd920320380e13d
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-07-09 17:54:49 +0530

    feat: add dummy data and page layout

---
+++ website/src/data/users.jsx
@@ -0,0 +1,75 @@
+/*
+Welcome to submissions for Mockbee.
+
+Below you will see an array of objects. Each object signifies a project currently shown at the <url>/showcase. 
+You can add yours below , by simply creating an object below with the exact same keys, but your own values.
+For example, an Adarsh Balika would make the following object :
+{
+  title:"Principles of Javascript",
+  description:"Javascript is the best , I dont like anything else.",
+  preview: require("./showcase/<adarsh-image.jpg>"),
+  source:"https://github.com/<adarsh-balika-repo>",
+  tags :["Adarsh","Balika"]
+}
+Here's and example of a good PR :<link>
+
+There are two ways to create a PR: 
+1.Github UI Interface
+2.Forking the repo and making the changes locally then commiting them and creating a PR.
+
+A few steps to be followed :
+
+1.Github UI Interface.
+ * - Create a PR in this file of Users.
+ * - This will create a branch on your Docusaurus fork (usually "patch-1").
+ * - Rename the branch to addtoshowcase/<yourprojecttitle>
+ * - Go to https://github.com/sashaboi/docasaurus-testing/tree/master/src/data/showcase
+ * - Drag-and-drop an image here to add it to your existing PR. <dimensions to be followed>
+ 
+2.Forking.
+
+ * - Create a branch with name which follows addtoshowcase/<yourprojecttitle>
+ * - Make changes to users.jsx , and add your image to the docasaurus-testing/tree/master/src/data/showcase folder.
+ * - Commit to your own branch and create a pull request to merge to the <whichever we want> Master branch
+ * 
+
+After submission , our maintainers will take 3-5 weeks to review, process, approve your applications and then merge your changes into the production branch.
+Thank you for submitting!
+
+*/
+
+export const users = [
+    {
+      title: "AgileTs",
+      description: "Global State and Logic Framework for reactive Applications",
+      preview: require("./showcase/agilets.png"),
+      website: "https://agile-ts.org/",
+      source: "https://github.com/agile-ts/documentation",
+      tags: ["opensource", "design"],
+    },
+    {
+      title: "AgileTs",
+      description: "Global State and Logic Framework for reactive Applications",
+      preview: require("./showcase/agilets.png"),
+      website: "https://agile-ts.org/",
+      source: "https://github.com/agile-ts/documentation",
+      tags: ["opensource", "design"],
+    },
+    {
+      title: "AgileTs",
+      description: "Global State and Logic Framework for reactive Applications",
+      preview: require("./showcase/agilets.png"),
+      website: "https://agile-ts.org/",
+      source: "https://github.com/agile-ts/documentation",
+      tags: ["opensource", "design"],
+    },
+    {
+      title: "AgileTs",
+      description: "Global State and Logic Framework for reactive Applications",
+      preview: require("./showcase/agilets.png"),
+      website: "https://agile-ts.org/",
+      source: "https://github.com/agile-ts/documentation",
+      tags: ["opensource", "design"],
+    },
+  ];
+  
--- website/src/pages/showcase/index.jsx
+++ website/src/pages/showcase/index.jsx
@@ -1,13 +1,80 @@
-import React from 'react';
+import React from "react";
+import Translate from "@docusaurus/Translate";
 import Layout from "@theme/Layout";
+import { users } from "../../data/users";
+import ShowcaseCard from "../../components/ShowcaseCard/ShowcaseCard";
 import "./showcase.css";
+import FilterChip from "../../components/FilterChip/FilterChip";
+
+const filters = [
+  {
+    id: 1,
+    name: "Favourite",
+    color: "pink",
+  },
+  {
+    id: 1,
+    name: "Open Source",
+    color: "red",
+  },
+  {
+    id: 1,
+    name: "Product",
+    color: "green",
+  },
+  {
+    id: 1,
+    name: "Design",
+    color: "blue",
+  },
+];
 
 const ShowcaseHeader = () => {
-    return (
-        <div className='showcase-header'>
-            <h1>Showcase Page</h1>
+  return (
+    <section className="margin-top--lg margin-bottom--lg text--center">
+      <h1>Mockbee Showcase</h1>
+      <p>List of projetcs people are building with Mockbee</p>
+      <a
+        className="button button--primary"
+        href="#"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Translate id="showcase.header.button">
+          🙏 Please add your site
+        </Translate>
+      </a>
+    </section>
+  );
+};
+
+function ShowcaseFilters() {
+  return (
+    <section className="container margin-top--l margin-bottom--lg">
+      <div className="margin-bottom--sm">
+        <div>
+          <h2>
+            <Translate id="showcase.filters.title">Filters</Translate>
+          </h2>
         </div>
-    )
+      </div>
+      <ul className="clean-list filter-container">
+        {filters.map((filter) => (
+          <FilterChip key={filter.id} filter={filter} />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function ShowcaseCards() {
+  return (
+    <div className="showcase-cards-container container margin-top--l margin-bottom--lg">
+      {users.map((user) => (
+        <ShowcaseCard user={user} />
+      ))}
+    </div>
+  );
 }
 
 const Showcase = () => {
@@ -15,6 +82,8 @@ const Showcase = () => {
    <Layout  title={`Showcase`}
    description="Description will go into a meta tag in <head />" >
     <ShowcaseHeader />
+    <ShowcaseFilters />
+    <ShowcaseCards />
    </Layout>
   )
 }
